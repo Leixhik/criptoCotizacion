df082a75acb76131e1b969a0232a66dd83295c27
Leixhik <eduardo.leijapl@uanl.edu.mx>
2025-12-03 11:37:07 -0600

    Hice Refactor del código utilizando Composable en un useCripto, para mejorar la obtención de datos y la gestión de errores

--- src/App.vue
+++ src/App.vue
@@ -1,66 +1,34 @@
 <script setup>
-  import { ref, reactive, onMounted, computed } from "vue";
-  import Alerta from "./components/Alerta.vue";
-  import Spinner from "./components/Spinner.vue";
-  import useCripto from "./composables/useCripto";
+import { ref, reactive, computed } from "vue";
+import Alerta from "./components/Alerta.vue";
+import Spinner from "./components/Spinner.vue";
+import useCripto from "./composables/useCripto";
 
-  const {} = useCripto()
+const {
+  monedas,
+  criptomonedas,
+  cotizacion,
+  cargando,
+  obtenerCotizacion,
+  tieneCotizacion,
+} = useCripto();
 
-  const monedas = ref([
-      { codigo: 'USD', texto: 'Dolar de Estados Unidos'},
-      { codigo: 'MXN', texto: 'Peso Mexicano'},
-      { codigo: 'EUR', texto: 'Euro'},
-      { codigo: 'GBP', texto: 'Libra Esterlina'},
-  ])
+const error = ref("");
 
-  const criptomonedas = ref([])
-  const error = ref('')
+const cotizar = reactive({
+  moneda: "",
+  criptomoneda: "",
+});
 
-  const cotizar = reactive({
-    moneda: '',
-    criptomoneda: ''
-  })
-
-  const cotizacion = ref({})
-  const cargando = ref(false)
-
-  onMounted(() => {
-    const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD'
-    fetch(url)
-      .then(respuesta => respuesta.json())
-      .then(({Data}) => criptomonedas.value = Data)
-  })
-
-  const cotizarCripto = () => {
-    // Validar que cotizar esté lleno
-    if (Object.values(cotizar).includes('')) {
-      error.value = 'Todos los campos son obligatorios'
-      return
-    }
-    error.value = ''
-    obtenerCotizacion()
+const cotizarCripto = () => {
+  // Validar que cotizar esté lleno
+  if (Object.values(cotizar).includes("")) {
+    error.value = "Todos los campos son obligatorios";
+    return;
   }
-
-  const obtenerCotizacion = async () => {
-    cargando.value = true
-    cotizacion.value = {}
-    try {
-      const { moneda, criptomoneda } = cotizar
-      const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`
-
-      const respuesta = await fetch(url)
-      const data = await respuesta.json()
-
-      cotizacion.value = data.DISPLAY[criptomoneda][moneda]
-    } catch (error) {
-      console.log(error)
-    } finally {
-      cargando.value = false
-    }
-  }
-
-  const tieneCotizacion = computed(() => Object.values(cotizacion.value).length > 0)
-
+  error.value = "";
+  obtenerCotizacion(cotizar);
+};
 </script>
 
 <template>
@@ -68,78 +36,75 @@
     <h1 class="titulo">Cotizador de <span>Criptomonedas</span></h1>
 
     <div class="contenido">
-      <Alerta
-        v-if="error"
-      >{{ error }}</Alerta>
+      <Alerta v-if="error">{{ error }}</Alerta>
 
-        <form
-          class="formulario"
-          @submit.prevent="cotizarCripto"
+      <form class="formulario" @submit.prevent="cotizarCripto">
+        <div class="campo">
+          <label class="moneda">Moneda:</label>
+          <select id="moneda" v-model="cotizar.moneda">
+            <option value="">-- Selecciona --</option>
+            <option v-for="moneda in monedas" :value="moneda.codigo">
+              {{ moneda.texto }}
+            </option>
+          </select>
+        </div>
 
-        >
-          <div class="campo">
-            <label class="moneda">Moneda:</label>
-            <select
-              id="moneda"
-              v-model="cotizar.moneda"
+        <div class="campo">
+          <label class="cripto">Criptomoneda:</label>
+          <select id="cripto" v-model="cotizar.criptomoneda">
+            <option value="">-- Selecciona --</option>
+            <option
+              v-for="criptomoneda in criptomonedas"
+              :value="criptomoneda.CoinInfo.Name"
             >
-              <option value=""> -- Selecciona --</option>
-              <option
-                v-for="moneda in monedas"
-                :value="moneda.codigo"
-              >{{ moneda.texto }}</option>
-            </select>
-          </div>
+              {{ criptomoneda.CoinInfo.FullName }}
+            </option>
+          </select>
+        </div>
 
-          <div class="campo">
-            <label class="cripto">Criptomoneda:</label>
-            <select
-              id="cripto"
-              v-model="cotizar.criptomoneda"
-            >
-              <option value=""> -- Selecciona --</option>
-              <option
-                v-for="criptomoneda in criptomonedas"
-                :value="criptomoneda.CoinInfo.Name"
-              >{{ criptomoneda.CoinInfo.FullName }}</option>
-            </select>
-          </div>
+        <input type="submit" value="Cotizar" />
+      </form>
+      <Spinner class="spinner" v-if="cargando" />
 
-          <input type="submit" value="Cotizar" />
+      <div v-if="tieneCotizacion" class="contenedor-resultado">
+        <h2>Cotización</h2>
 
-        </form>
-        <Spinner
-          class="spinner"
-          v-if="cargando"
-        />
-
-        <div v-if="tieneCotizacion" class="contenedor-resultado">
-          <h2>Cotización</h2>
-
-          <div class="resultado">
-            <img
-              :src="'https://cryptocompare.com/' + cotizacion.IMAGEURL"
-               alt="imagen cripto">
-            <div>
-              <p>El precio es de: <span>{{ cotizacion.PRICE }}</span></p>
-              <p>El precio más alto del día: <span>{{ cotizacion.HIGHDAY }}</span></p>
-              <p>El precio más bajo del día: <span>{{ cotizacion.LOWDAY }}</span></p>
-              <p>Variación últimas 24 horas: <span>{{ cotizacion.CHANGEPCT24HOUR }}</span></p>
-              <p>Última Actualización: <span>{{ cotizacion.LASTUPDATE }}</span></p>
-            </div>
+        <div class="resultado">
+          <img
+            :src="'https://cryptocompare.com/' + cotizacion.IMAGEURL"
+            alt="imagen cripto"
+          />
+          <div>
+            <p>
+              El precio es de: <span>{{ cotizacion.PRICE }}</span>
+            </p>
+            <p>
+              El precio más alto del día: <span>{{ cotizacion.HIGHDAY }}</span>
+            </p>
+            <p>
+              El precio más bajo del día: <span>{{ cotizacion.LOWDAY }}</span>
+            </p>
+            <p>
+              Variación últimas 24 horas:
+              <span>{{ cotizacion.CHANGEPCT24HOUR }}</span>
+            </p>
+            <p>
+              Última Actualización: <span>{{ cotizacion.LASTUPDATE }}</span>
+            </p>
           </div>
         </div>
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped>
-  .contenido {
-    display:flex;
-    flex-direction: column;
-    align-items: center;
-  }
-  .spinner {
-    margin-top: 2rem;
-  }
+.contenido {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.spinner {
+  margin-top: 2rem;
+}
 </style>
